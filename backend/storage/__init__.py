e28d6beeec5c1eb8c23502dd7cb2343e25d59a11
Chris Seto <Chriskseto@gmail.com>
2014-05-05 14:20:38 -0400

    Placeholders.

---
+++ backend/storage/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module: Storage
+A dynamic storage module
+Implements four (4) methods:
+    put_directory
+    put_file
+    get_directory
+    get_file
+"""
---
+++ foreman.py
@@ -0,0 +1,18 @@
+"""
+foreman.py
+a module for passing tasks to celery workers
+"""
+from celery import Celery
+
+
+#TODO
+def push_task(node):
+    pass
+
+
+def partition_task(node):
+    pass
+
+
+def queue_task(task, **kwargs):
+    pass
