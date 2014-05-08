366de6cf9e7b148b20fa65de49e7d257a102d5f1
Chris Seto <Chriskseto@gmail.com>
2014-05-08 13:35:49 -0400

    Update tasks

---
+++ registerer/tasks/__init__.py
@@ -0,0 +1,1 @@
+from registerer.tasks import callbacks, exceptions, management, registration
--- tasks.py
+++ tasks.py
@@ -9,3 +9,13 @@ def vagrant():
 @task
 def build_worker():
     print 'TODO'
+
+
+@task
+def worker():
+    run('celery -A registerer.celery worker -I registerer.tasks')
+
+
+@task
+def docker_worker():
+    run('celery -A registerer.celery worker -I registerer.tasks -b $SERVICE_PORT_5672_TCP_ADDR')
