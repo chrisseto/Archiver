0962bd540ce25309d7f02bf552fdb54c5ec06d76
Nan Chen <nc6r@virginia.edu>
2014-05-10 02:48:11 -0400

    add

--- registerer/backend/database/__init__.py
+++ registerer/backend/database/__init__.py
@@ -6,3 +6,43 @@ This includes but is not limitted to:
     Tracking the status of current nodes being registered
 
 """
+from pymongo import MongoClient
+from bson import ObjectId
+
+from ...settings import defaults
+
+mongo_uri = 'mongodb://localhost:{port}'.format(port=defaults.DB_PORT)
+client = MongoClient(mongo_uri)
+
+db = client[defaults.DB_NAME]
+
+if defaults.DB_USER and defaults.DB_PASSWORD:
+    db.authenticate(defaults.DB_USER, defaults.DB_PASSWORD)
+
+
+def set_up_storage(schemas, storage_class, prefix='', addons=None, *args, **kwargs):
+    '''Setup the storage backend for each schema in ``schemas``.
+    note::
+        ``**kwargs`` are passed to the constructor of ``storage_class``
+
+    Example usage with modular-odm and pymongo:
+    ::
+
+        >>> from pymongo import MongoClient
+        >>> from modularodm.storage import MongoStorage
+        >>> client = MongoClient(port=20771)
+        >>> db = client['mydb']
+        >>> models = [User, Node, addons]
+        >>> set_up_storage(models, MongoStorage, db=db)
+    '''
+    # import pdb; pdb.set_trace()
+    _schemas = []
+    _schemas.extend(schemas)
+
+    for addon in (addons or []):
+        _schemas.extend(addon.models)
+
+    for schema in _schemas:
+        collection = "{0}{1}".format(prefix, schema._name)
+        schema.set_storage(storage_class(collection=collection, **kwargs))
+    return None
--- registerer/settings/defaults.py
+++ registerer/settings/defaults.py
@@ -3,3 +3,19 @@ RABBITMQ_ADDRESS = 'amqp://guest:guest@192.168.33.10//'
 
 #### CELERY OPTIONS ####
 CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = "mongodb"
+CELERY_MONGODB_BACKEND_SETTINGS = {
+    # Shouldn't need these - defaults are correct.
+    "host": "localhost",
+    "port": 27017,
+    "database": "celery",
+    "taskmeta_collection": "messages",
+}
+
+#### mongodb OPTIONS ####
+BROKER_BACKEND = 'mongodb'
+BROKER_HOST = "localhost"
+BROKER_PORT = 27017
+BROKER_USER = ""
+BROKER_PASSWORD = ""
+BROKER_VHOST = ""
