f3f1cc0b37f3f2430a2379ea33a3685e25c5be78
Chris Seto <Chriskseto@gmail.com>
2014-05-15 16:04:06 -0400

    Update/finish/fix Dropbox cloner

--- archiver/worker/tasks/archival/dropbox_clone.py
+++ archiver/worker/tasks/archival/dropbox_clone.py
@@ -1,37 +1,46 @@
+import os
+
 from dropbox.client import DropboxClient
 
 from archiver import celery
 from archiver.backend import push_file
 
-from .utils import CUTOFF_SIZE, chunked_save
+from .utils import CUTOFF_SIZE, chunked_save, ensure_directory
 
 
-def clone_dropbox(addon):
-    client = DropboxClient(addon['access_token'])
-    folder = client.metadata(addon['folder'])
-    recurse(folder['contents'], client)
+def clone(addon):
+    addon.make_dir(addon['folder'])
+    return clone_dropbox(addon['access_token'], addon['folder'], addon.parent.TEMP_DIR)
 
 
-def recurse(contents, client, addon):
+def clone_dropbox(access_token, folder, savedir_prefix):
+    client = DropboxClient(access_token)
+    folder = client.metadata(folder)
+    recurse(folder['contents'], client, savedir_prefix)
+
+
+def recurse(contents, client, savedir_prefix):
     for item in contents:
         if item['is_dir']:
             if item['bytes'] > CUTOFF_SIZE:
-                recurse.delay(contents, client, addon)
+                recurse.delay(contents, client, savedir_prefix)
             else:
-                recurse(contents, client)
+                recurse(contents, client, savedir_prefix)
         if item['bytes'] > CUTOFF_SIZE:
-            fetch.delay(item['path'], client, addon)
+            fetch.delay(item['path'], client, savedir_prefix)
         else:
-            fetch(item['path'], client, addon)
+            fetch(item['path'], client, savedir_prefix)
 
 
 @celery.task
-def fetch(path, client, addon):
+def fetch(path, client, savedir_prefix):
     fobj = client.get_file(path)
-    to_loc = addon.full_path(path)
-    chunked_save(fobj, )
-    push_file(to_loc, addon.path(path))
 
+    path = path[1:]  # Remove begining /
+    full_dir = os.path.join(savedir_prefix, os.path.dirname(path))
+    full_path = os.path.join(savedir_prefix, path)
 
-#export as clone
-clone = clone_dropbox
+    ensure_directory(full_dir)
+    chunked_save(fobj, full_path)
+
+    push_file(full_path, path)
