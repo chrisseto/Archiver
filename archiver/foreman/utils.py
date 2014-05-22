b85d007de9a10fd2f67a60c11e885cba310f1ee7
Chris Seto <Chriskseto@gmail.com>
2014-05-22 14:51:46 -0400

    Makes foreman report inability to connect to rabbit

--- archiver/foreman/utils.py
+++ archiver/foreman/utils.py
@@ -1,4 +1,5 @@
 import httplib as http
+from socket import error as SocketError
 
 from flask import jsonify
 
@@ -22,6 +23,16 @@ def push_task(node):
         ret = jsonify({'response': ret})
         ret.status_code = http.CREATED
 
+    except SocketError as e:
+        if e.errno == 54:
+            ret.update(
+                {'status': 'ERROR',
+                 'reason': 'could not connect to rabbitmq'
+                 })
+            ret = jsonify({'response': ret})
+            ret.status_code = http.INTERNAL_SERVER_ERROR
+        else:
+            raise
     except Exception:
         ret.update({'status': 'ERROR'})
         ret = jsonify({'response': ret})
