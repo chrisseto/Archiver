9d7654bb5239d0857de0961eba0b574ddfaba55b
Chris Seto <Chriskseto@gmail.com>
2014-06-10 10:01:55 -0400

    Fixes a s3 test

--- tests/test_archive_s3.py
+++ tests/test_archive_s3.py
@@ -45,13 +45,16 @@ def test_iters_bucket_list(monkeypatch, s3_service, bucket):
     mock_get_key.assert_has_calls(kalls[:2], any_order=True)
 
 
-@pytest.mark.usesfixtures('patch_push')
-def test_pushes_files(s3_service, push_file):
+@pytest.mark.usesfixtures('push_file')
+@pytest.mark.usesfixtures('push_json')
+def test_pushes_files(s3_service, push_file, push_json):
     mock_key = MockKey()
     archiver = S3Archiver(s3_service)
     key, meta = archiver.get_key(archiver, mock_key)
     assert key == mock_key
-    assert len(push_file.mock_calls) == 2
+    assert len(push_file.mock_calls) == 1
+    assert len(push_json.mock_calls) == 1
+    assert push_json.called
 
 
 def test_resource_pulled(s3_service):
