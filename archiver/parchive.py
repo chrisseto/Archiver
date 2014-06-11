d92eec647c481341e65fd275e91fb351333d4820
Chris Seto <Chriskseto@gmail.com>
2014-06-11 10:56:38 -0400

    updates parity module

--- archiver/parchive.py
+++ archiver/parchive.py
@@ -1,22 +1,64 @@
 import os
 import glob
+import logging
+import hashlib
 import subprocess
 
 PAR2BIN = 'par2'
 PARITY_CUT_OFF_SIZE = 100 * 1024  # 100kb
 
+logger = logging.getLogger(__name__)
 
-def create_parity_files(path, force=False):
+
+class ParchiveException(Exception):
+    pass
+
+
+def _metadata(path):
+    sha = hashlib.sha256()
+    md5 = hashlib.md5()
+    with open(path) as parity:
+        while True:
+            chunk = parity.read(1024)
+            if chunk:
+                sha.update(chunk)
+                md5.update(chunk)
+            else:
+                break
+    return {
+        'sha256': sha.hexdigest(),
+        'md5': md5.hexdigest(),
+        'lastModified': os.path.getmtime(path),
+        'size': os.path.getsize(path)
+    }
+
+
+def build_parity_metadata(paths):
+    meta = {}
+    for path in paths:
+        meta[os.path.basename(path)] = _metadata(path)
+    return meta
+
+
+def create_parity_files(path, name, redundancy=5, force=False, files=1):
     if not force and os.path.getsize(path) < PARITY_CUT_OFF_SIZE:
-        return False
-    if subprocess.call([PAR2BIN, 'c', '-n1', path]) == 0:
+        logger.info('Skipping parity creation for "%s", too small.' % name)
+        return []
+    folder_path = os.path.dirname(path)
+    if subprocess.call([
+        PAR2BIN,
+        'c',
+        '-n{}'.format(files),
+        '-r{}'.format(redundancy),
+        os.path.join(folder_path, '%s.par2' % name),
+            path]) == 0:
         return [
             os.path.abspath(fpath)
             for fpath in
-            glob.glob('*.par2')
-            if path in fpath
+            glob.glob(os.path.join(folder_path, '*.par2'))
+            if name in fpath
         ]
-    return False
+    raise ParchiveException()
 
 
 def verify_parity_files():
