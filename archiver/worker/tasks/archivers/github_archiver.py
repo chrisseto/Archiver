48955ecd94dd3806bb4a45ed008c594fdbfc8fd0
Nan Chen <nc6r@virginia.edu>
2014-06-23 11:36:47 -0400

    update

--- archiver/worker/tasks/archivers/github_archiver.py
+++ archiver/worker/tasks/archivers/github_archiver.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from git import Git
 
@@ -11,6 +12,7 @@ from archiver.backend import store
 
 from base import ServiceArchiver
 
+logger = logging.getLogger(__name__)
 
 class GithubArchiver(ServiceArchiver):
     ARCHIVES = 'github'
@@ -28,12 +30,9 @@ class GithubArchiver(ServiceArchiver):
         super(GithubArchiver, self).__init__(service)
 
     def clone(self):
-        path, save_loc = self.build_directories('')
-        Git().clone(self.url, path)
-        g = Git(path)
-        self.pull_all_branches(g)
-        self.sanitize_config(path)
-        store.push_directory(path, save_loc)
+        header = self.build_header(self.url, self.versions)
+        logging.info('Archiving {} items from "{}"'.format(len(header), self.url))
+        return chord(header, self.clone_done.s(self))
 
     def sanitize_config(self, path):
         git_path = os.path.join(path, '.git', 'config')
@@ -69,34 +68,28 @@ class GithubArchiver(ServiceArchiver):
         git.fetch('--all')
         git.pull('--all')
 
-    def build_header(self, branch, versions=None):
-        header = []
-        for item in self.client.metadata(branch)['contents']:
-            if item['is_dir']:
-                header.append(self.build_header(item['path'], versions=versions))
-            else:
-                header.append(self.build_file_chord(item, versions=versions))
-        return header
-
-    def build_file_chord(self, item, versions=None):
-        if not versions:
-            return self.fetch.si(self, item['path'], rev=None)
-        header = []
-        for rev in self.client.revisions(item['path'], versions):
-            header.append(self.fetch.si(self, item['path'], rev=rev['rev']))
-        return chord(header, self.file_done.s(self, item['path']))
-
-    @celery.task
-    def fetch(self, path, rev=None):
-        fobj, metadata = self.client.get_file_and_metadata(path, rev)
-        tpath = self.chunked_save(fobj)
+    def build_header(self, url, versions=None):
+        fobj, path = self.get_temp_file(url, versions)
         fobj.close()
-        lastmod = self.to_epoch(parser.parse(metadata['modified']))
-        metadata = self.get_metadata(tpath, path)
+        Git().clone(self.url, path)
+        g = Git(path)
+        self.pull_all_branches(g)
+        self.sanitize_config(path)
+        lastmod = self.to_epoch(self.url.last_modified)
+        metadata = self.get_metadata(url, path)
         metadata['lastModified'] = lastmod
-        store.push_file(tpath, metadata['sha256'])
+        store.push_file(url, metadata['sha256'])
         store.push_metadata(metadata, metadata['sha256'])
         return metadata
+
+
+    def build_file_chord(self, url, versions=None):
+        if not versions:
+            return self.fetch.si(self, url, rev=None)
+        header = []
+        for rev in self.client.revisions(url, versions):
+            header.append(self.fetch.si(self, url, rev=rev['rev']))
+        return chord(header, self.file_done.s(self, url))
 
     @celery.task
     def file_done(rets, self, path):
