4cb26db88eeaad7979fedc7218e41dc2abce3131
Chris Seto <Chriskseto@gmail.com>
2014-07-10 11:11:47 -0400

    Log callbacks to the foreman

--- archiver/foreman/views.py
+++ archiver/foreman/views.py
@@ -1,24 +1,32 @@
 import os
+import json
+import logging
+import httplib as http
 
 from flask import request, jsonify, Blueprint
 
 from archiver import settings
+from archiver.util import signing
 from archiver.backend import store
 from archiver.datatypes import Container
-from archiver.exceptions import ValidationError
-
+from archiver.exceptions import ValidationError, HTTPError
 
 from utils import push_task
 
 
+logger = logging.getLogger(__name__)
 rest = Blueprint('archiver', __name__)
 
 
 @rest.route('/', methods=['POST', 'PUT'])
 def begin_register():
-    json = request.get_json(force=True)
-    if json:
-        container = Container.from_json(json)
+    request_json = request.get_json(force=True)
+
+    if settings.REQUIRE_SIGNED_SUBMITIONS and not signing.verify_submition(request_json):
+        raise HTTPError(http.UNAUTHORIZED)
+
+    if request_json:
+        container = Container.from_json(request_json)
         # Container should always be defined otherwise a
         # validation error will be raised by from_json
         if container:
@@ -35,12 +43,28 @@ def list_projects():
 
 @rest.route('/callback', methods=['POST', 'PUT'])
 def callback():
-    json = request.get_json(force=True)
-    if json:
-        print json
-        return ''
-    print "No data in callback"
-    raise ValidationError('no data')
+    callback_json = request.get_json(force=True)
+
+    if not signing.verify_callback(callback_json):
+        logger.warn('Incorrectly signed callback from %s' %
+                    request.environ['REMOTE_ADDR'])
+        raise HTTPError(http.UNAUTHORIZED)
+
+    try:
+        if callback_json['status'] == 'failed':
+            logger.warn('Failed to archive {} because {}.'.format(
+                callback_json['id'], ', '.join(callback_json['reasons'])))
+        elif callback_json['status'] == 'success':
+            logger.info('Successfully archived {} with failures {}. ({})'.format(
+                callback_json['id'], ', '.join(callback_json['failures'], len(callback_json['failures']))))
+        else:
+            logger.warning('Unknown status from %s' % request.environ['REMOTE_ADDR'])
+
+        logger.info('===Raw json===')
+        logger.info(json.dumps(callback_json, indent=4, sort_keys=True))
+        logger.info('===End json===')
+    except:
+        raise ValidationError('no data')
 
 
 @rest.route('/<string(maxlength=60):cid>')
