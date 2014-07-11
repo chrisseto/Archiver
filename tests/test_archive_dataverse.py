4f3d0f0dc9014fa36c127d670f0d0e84611903c2
Chris Seto <Chriskseto@gmail.com>
2014-07-11 15:56:17 -0400

    A few more dataverse tests

--- tests/test_archive_dataverse.py
+++ tests/test_archive_dataverse.py
@@ -5,6 +5,7 @@ import pytest
 from archiver import settings
 settings.CELERY_ALWAYS_EAGER = True
 
+from archiver import exceptions
 from archiver.datatypes import Container
 from archiver.worker.tasks.archivers import get_archiver
 from archiver.worker.tasks.archivers.dataverse_archiver import DataverseArchiver
@@ -12,18 +13,36 @@ from archiver.worker.tasks.archivers.dataverse_archiver import DataverseArchiver
 from utils import jsons
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def dataverse_service():
     return Container.from_json(jsons.good_with_dataverse).services[0]
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def dataverse_container():
     return Container.from_json(jsons.good_with_dataverse)
+
+
+@pytest.fixture
+def dvarchiver(dataverse_service):
+    return DataverseArchiver(dataverse_service)
 
 
 def test_gets_called():
     assert get_archiver('dataverse') == DataverseArchiver
     assert get_archiver('dataverse').ARCHIVES == 'dataverse'
 
-#TODO Finish testing
+
+def test_host_override(dataverse_service):
+    dataverse_service.raw_json['host'] = 'Myfakedataverse.com'
+    archiver = DataverseArchiver(dataverse_service)
+    assert archiver.host == 'Myfakedataverse.com'
+
+
+def test_exception_on_malform(dataverse_service):
+    del dataverse_service.raw_json['password']
+
+    with pytest.raises(exceptions.DataverseArchiverError) as e:
+        DataverseArchiver(dataverse_service)
+
+    assert 'password' in e.value.message
